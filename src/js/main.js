6669a700f29541c0e7a474765bfc4fa62bfc65c6
iewaw <izabelawawrzyniak90@gmail.com>
2024-01-04 21:22:36 +0100

    declaring first variables and fetching Disney characters data

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,17 @@
 
 console.log('Hello, world!');
 
+const characterList = document.querySelector('.js_list_all');
+const favouriteList = document.querySelector('.js_list_favourites');
+
+let allCharacters = [];
+let favouriteCharacters = [];
+let filteredCharacters = [];
+
+fetch('//api.disneyapi.dev/character?pageSize=50')
+  .then( response => response.json() )
+  .then( data => {
+ 
+    console.log(data);
+
+  });
