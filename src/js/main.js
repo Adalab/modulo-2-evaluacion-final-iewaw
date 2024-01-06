087bffcefeed4165c0832a1f67d0b3ddb5abc89c
iewaw <izabelawawrzyniak90@gmail.com>
2024-01-06 12:32:59 +0100

    rendering all characters

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-console.log('Hello, world!');
-
 const characterList = document.querySelector('.js_list_all');
 const favouriteList = document.querySelector('.js_list_favourites');
 
@@ -13,6 +11,20 @@ fetch('//api.disneyapi.dev/character?pageSize=50')
   .then( response => response.json() )
   .then( data => {
  
-    console.log(data);
+    allCharacters = data.data;
+
+    for(let character of allCharacters) {
+      renderCharacters(character);
+    }
 
   });
+
+function renderCharacters(character) {
+
+  const newCharacter = document.createElement('li');
+
+  newCharacter.insertAdjacentHTML('afterbegin', `<div class="characters__card"><img class="characters__card__img" src="${character.imageUrl ? character.imageUrl : 'https://via.placeholder.com/160x200/cfe2f3/351c75/?text=Disney'}" alt="Picture of ${character.name}"><h3 class="characters__card__name">${character.name}</h3></div>`);
+
+
+  characterList.appendChild(newCharacter);
+}
