ba35969b017a155aae87e5da0b7b85be3cdfd57b
iewaw <izabelawawrzyniak90@gmail.com>
2024-01-09 09:26:49 +0100

    adding the close button to the favourite cards

--- src/js/main.js
+++ src/js/main.js
@@ -22,7 +22,7 @@ searchButton.addEventListener('click', handleSearch);
 
 /**
  * function that allows to search a character by its name
- * @param event 
+ * @param event
  */
 function handleSearch(event) {
     event.preventDefault();
@@ -57,6 +57,29 @@ function handleClick(event) {
 
     //identify the character
     const clickedCharacter = event.currentTarget;
+    identifyCharacter(clickedCharacter);
+}
+
+/**
+ * function that handles the close button on favourite cards
+ * @param event
+ */
+function handleClose(event) {
+
+    //identify the character
+    const clickedCharacter = event.currentTarget.parentNode.parentNode;
+    identifyCharacter(clickedCharacter);
+}
+
+//OTHER FUNCTIONS
+
+/**
+ * function that identifies the character by its id
+ * @param clickedCharacter
+ */
+function identifyCharacter(clickedCharacter) {
+
+    //obtain the clicked card's id
     const clickedId = parseInt(clickedCharacter.dataset.id);
     const characterDiv = clickedCharacter.querySelector('.characters__card');
     let selectedCharacter;
@@ -68,12 +91,10 @@ function handleClick(event) {
 
             selectedCharacter = data.data;
 
+            //call the function that manages favourite cards
             manageFavourites(characterDiv, selectedCharacter);
         });
 }
-
-
-//OTHER FUNCTIONS
 
 /**
  * function that shows Disney characters on page load
@@ -101,12 +122,16 @@ function renderCharacters(character) {
     //create characters
     characterList.appendChild(createCharacter(character));
 
+
     //select all li elements
     const allCards = document.querySelectorAll('.js__character__card');
 
     //add event listeners to all li elements
     for (const card of allCards) {
-        card.addEventListener('click', handleClick);
+        if (!card.closest('.js_list_favourites')) {
+            card.addEventListener('click', handleClick);
+            card.classList.add('clickable');
+        }
     }
 }
 
@@ -124,7 +149,7 @@ function createCharacter(character) {
     newCharacter.classList.add('js__character__card');
 
     //create inner HTML content
-    newCharacter.insertAdjacentHTML('afterbegin', `<div class="characters__card"><img class="characters__card__img" src="${character.imageUrl ? character.imageUrl : 'https://via.placeholder.com/120x120/cfe2f3/351c75/?text=Disney'}" alt="Picture of ${character.name}"><h3 class="characters__card__name js__card_name">${character.name}</h3></div>`);
+    newCharacter.insertAdjacentHTML('afterbegin', `<div class="characters__card"><h3 class="characters__card__close js__card__close"></h3><img class="characters__card__img" src="${character.imageUrl ? character.imageUrl : 'https://via.placeholder.com/120x120/cfe2f3/351c75/?text=Disney'}" alt="Picture of ${character.name}"><h3 class="characters__card__name js__card__name">${character.name}</h3></div>`);
 
     return newCharacter;
 }
@@ -151,11 +176,10 @@ function manageFavourites(characterDiv, selectedCharacter) {
     const existingCharacter = favouriteList.querySelector(`[data-id="${selectedCharacter._id}"]`);
 
     //check conditions to apply the logic
-    if (!characterDiv.classList.contains('favourite') && !existingCharacter) {
+    if (!characterDiv.classList.contains('favourite') && !existingCharacter && characterDiv.closest('.js_list_all')) {
         characterDiv.classList.add('favourite');
         markFavourite(selectedCharacter);
-        favouriteCharacters.push(selectedCharacter);
-    } else if (!characterDiv.classList.contains('favourite') && existingCharacter) {
+    } else if (!characterDiv.classList.contains('favourite') && existingCharacter && characterDiv.closest('.js_list_all')) {
         characterDiv.classList.add('favourite');
     } else {
         characterDiv.classList.remove('favourite');
@@ -169,13 +193,29 @@ function manageFavourites(characterDiv, selectedCharacter) {
 }
 
 /**
- * function that marks favourite characters with a cat icon 😻
+ * function that marks and saves favourite cards
  * @param favourite
  */
 function markFavourite(favourite) {
+
+    //create new li element in favourites
     const newFavourite = createCharacter(favourite);
-    const newFavouriteName = newFavourite.querySelector('.js__card_name');
+
+    //add a cat icon to mark favourites
+    const newFavouriteName = newFavourite.querySelector('.js__card__name');
     const favouriteMark = document.createTextNode('😻');
     newFavouriteName.appendChild(favouriteMark);
+
+    //add a close button to the favourite card
+    const newFavouriteClose = newFavourite.querySelector('.js__card__close');
+    const closeButton = document.createTextNode('❌');
+    newFavouriteClose.appendChild(closeButton);
+    newFavouriteClose.classList.add('clickable');
+
+    //add an event listener to the close button
+    newFavouriteClose.addEventListener('click', handleClose);
     favouriteList.appendChild(newFavourite);
+
+    //save the favourite character to the favourite list
+    favouriteCharacters.push(favourite);
 }
